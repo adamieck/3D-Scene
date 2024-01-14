644a6ba97c16a11e218e036ac6cd514c0f59684d
adamieck <KAdamiec196@gmail.com>
2024-01-14 19:02:55 +0100

    added mesh class

---
+++ src/Mesh.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+#include "Shader.h"
+#include "VBLayout.h"
+#include "glm/glm.hpp"
+
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 texCoords;
+	glm::vec3 normal;	
+};
+
+struct Texture
+{
+	unsigned int id;
+	std::string type;
+};
+
+class Mesh
+{
+public:
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices,
+		std::vector<Texture> textures);
+	void Draw(Shader& shader);
+
+private:
+	// render
+	unsigned int VAO, VBO, EBO;
+	VBLayout layout;
+
+	void setupMesh();
+};
